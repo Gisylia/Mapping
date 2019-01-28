297ffd14d270015b5bc13ef5b35408dc0669bbbf
2geyog68 <2geyog68@solent.ac.uk>
2019-01-28 14:10:47 +0000

    Initial commit

---
+++ app/src/main/java/uk/ac/solent/mapping/MainActivity.java
@@ -0,0 +1,28 @@
+package uk.ac.solent.mapping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // add in the mapping view
+      MapView mv = findViewById(R.id.map1);
+
+      mv.setBuiltInZoomControls(true);
+      mv.setClickable(true);
+
+      mv.getController().setZoom(14);
+      mv.getController().setCenter(new GeoPoint(51.05, -0.72));
+
+
+
+    }
+}
