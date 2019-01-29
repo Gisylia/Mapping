74c214e14b1612c0522a8d4bba1e47263629faf2
2geyog68 <2geyog68@solent.ac.uk>
2019-01-29 14:49:52 +0000

    mapping app redone

--- app/src/main/java/uk/ac/solent/mapping/MainActivity.java
+++ app/src/main/java/uk/ac/solent/mapping/MainActivity.java
@@ -2,6 +2,11 @@ package uk.ac.solent.mapping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
@@ -13,27 +18,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // this is calling the btn action from the activity main
+        Button b = (Button) findViewById(R.id.btn1);
+        b.setOnClickListener((OnClickListener) this);
+
+
         /* add in the mapping view this will loccate the long/lat of the given
         location usind the activity.main page as well
          */
+        MapView mv = findViewById(R.id.map1);
 
-
-      MapView mv = findViewById(R.id.map1);
-
-      mv.setBuiltInZoomControls(true);
-      mv.setClickable(true);
-
-      mv.getController().setZoom(14);
-      mv.getController().setCenter(new GeoPoint(51.05, -0.72));
-
-
-
-      // btn area
-
-
-
-
+        mv.setBuiltInZoomControls(true);
+        mv.setClickable(true);
+        mv.getController().setZoom(14);
+        mv.getController().setCenter(new GeoPoint(51.05, -0.72));
 
 
     }
+
+
+    public void onClick(View view) {
+        // this is from the activity main
+        TextView tv = (TextView) findViewById(R.id.tv1);
+        EditText et = (EditText) findViewById(R.id.et1);
+
+
+    }
+
 }
