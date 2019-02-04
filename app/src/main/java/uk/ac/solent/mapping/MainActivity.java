4780fe7897ad6b81fc1ffa0ca2020f69242c9c28
2geyog68 <2geyog68@solent.ac.uk>
2019-02-04 13:48:41 +0000

    session5

--- app/src/main/java/uk/ac/solent/mapping/MainActivity.java
+++ app/src/main/java/uk/ac/solent/mapping/MainActivity.java
@@ -2,18 +2,22 @@ package uk.ac.solent.mapping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
 
 //ensure that the main activity implements the stuff on the activity main page
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button b = (Button) findViewById(R.id.btn1);
         // listener for the button
         b.setOnClickListener(this);
-
 
         /* add in the mapping view this will locate the long/lat of the given
         location used in the activity.main page as well
@@ -37,6 +40,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mv.getController().setCenter(new GeoPoint(51.05, -0.72));
     }
 
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.Menu.menu, menu);
+
+        return true;
+    }
 
     public void onClick(View view) {
         // this is from the activity main
